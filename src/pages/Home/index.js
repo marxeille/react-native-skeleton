272f16359b25a812cfbeabe6ef41ace4c305c5df
Son Lai <son.lha@agiletech.vn>
2020-06-03 08:55:04 +0700

    init hook-form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,21 @@
-import React, { useCallback } from 'react';
-import useService from '../../hooks/useService';
-import { getHomeDetail, updateHomeDetail, getListHome } from './services';
-import { View, Alert, TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react'; // useCallback
+// import useService from '../../hooks/useService';
+// import { getHomeDetail, updateHomeDetail, getListHome } from './services';
+import { TextInput, View, Button } from 'react-native';
 import Text from '../../components/Text';
-import FlatListService from '../../components/FlatListService';
-import { wrap } from '../../themes';
+// import FlatListService from '../../components/FlatListService';
+import { connect } from 'react-redux';
+import { useForm } from 'react-hook-form';
 
-const HomeScreen = wrap(() => {
+const HomeScreen = props => {
+  // const { dispatch } = props;
+  const { register, handleSubmit, setValue } = useForm();
+
+  useEffect(() => {
+    register('username');
+    register('password');
+  }, [register]);
+  // console.log('props', user);
   // const [loading, homeProperty] = useService(getHomeDetail, { id: '3' });
 
   // const onSubmit = useCallback(async () => {
@@ -21,15 +30,15 @@ const HomeScreen = wrap(() => {
   //   }
   // }, []);
 
-  const renderItem = useCallback(({ item }) => {
-    console.log('item', item);
-    return <Text style={{ color: 'yellow', height: 100 }}>{item}</Text>;
-  }, []);
+  // const renderItem = useCallback(({ item }) => {
+  //   console.log('item', item);
+  //   return <Text style={{ color: 'yellow', height: 100 }}>{item}</Text>;
+  // }, []);
 
-  const keyExtractor = useCallback(item => {
-    // console.log('item', item);
-    return item;
-  }, []);
+  // const keyExtractor = useCallback((item) => {
+  //   // console.log('item', item);
+  //   return item;
+  // }, []);
 
   const onSubmit = ({ username, password }) => {
     console.log(username, password);
@@ -37,30 +46,32 @@ const HomeScreen = wrap(() => {
 
   // if (homeProperty.id) {
   return (
-    <View cls="flx-i bg-red">
-      <TouchableOpacity
-        onPress={() => dispatch({ type: 'user/login', username: 'name', password: 'pass' })}
-        style={{ flex: 1, backgroundColor: 'red' }}>
-        <Text style={{ color: 'yellow', height: 100 }}>Login</Text>
-        {/* <FlatListService
+    <View
+      // onPress={() => dispatch({ type: 'user/login', username: 'name', password: 'pass' })}
+      style={{ flex: 1, backgroundColor: 'red' }}>
+      <Text style={{ color: 'yellow', height: 100 }}>Login</Text>
+      <TextInput
+        style={{ width: 100, height: 50 }}
+        onChangeText={value => setValue('username', value)}
+        placeholder="name"
+      />
+      <TextInput
+        style={{ width: 100, height: 50 }}
+        onChangeText={value => setValue('password', value)}
+        placeholder="pass"
+      />
+      {/* <FlatListService
         style={{ flex: 1, backgroundColor: 'blue' }}
         service={getListHome}
         params={{ name: 'name' }}
         renderItem={renderItem}
         keyExtractor={keyExtractor}
       /> */}
-      </TouchableOpacity>
-      <FlatListService
-        style={{ flex: 1 }}
-        service={getListHome}
-        params={{ name: 'name' }}
-        renderItem={renderItem}
-        keyExtractor={keyExtractor}
-      />
+      <Button title={'Login'} onPress={handleSubmit(onSubmit)} />
     </View>
   );
   // }
   // return null;
-});
+};
 
-export default HomeScreen;
+export default connect(state => ({ user: state.user }))(HomeScreen);
